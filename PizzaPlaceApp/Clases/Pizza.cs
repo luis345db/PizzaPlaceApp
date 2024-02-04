8a08e69aaab87cbe020c0d50f818c78d51dd1a61
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-02-04 17:04:21 -0600

    Agregue Imagenes Cambie algunos nombres de variables, por problemas con la misma conexion de las imagenes, Cree el Home y subi cada una de las imagenes

--- PizzaPlaceApp/Clases/Pizza.cs
+++ PizzaPlaceApp/Clases/Pizza.cs
@@ -6,14 +6,14 @@
         public int Id { get; }
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
-        public Picante Picantes { get; set; }
+        public Picante Picantez { get; set; }
         public Pizza(int id, string nombre, decimal precio,
         Picante picantes)
         {
             Id = id;
             Nombre = nombre;
             Precio = precio;
-            Picantes = picantes;
+            Picantez = picantes;
         }
 
     }
